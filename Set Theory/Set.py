db2a12d2e9fd9c972d80c3eeb47c4fb18530ebff
scprogramming <scottc130@gmail.com>
2020-07-17 13:55:16 -0400

    Refactoring structure

--- Set Theory/Set.py
+++ Set Theory/Set.py
@@ -1,15 +1,18 @@
 class Set:
-
+    #initalization
     def __init__(self):
         self.values = dict()
 
+    #Insert a value into the set
     def insert(self,value):
         if value not in self.values.keys():
             self.values[value] = value
 
+    #Remove a value from the set
     def remove(self,value):
         del self.values[value]
 
+    #Print the contents of the set
     def __str__(self):
         contents = "{"
         i =0
@@ -22,20 +25,26 @@ class Set:
         contents += "}"
         return contents
 
+    #iterate the set
     def __iter__(self):
         return iter(self.values.copy())
 
+    #determine the size of the set
     def __len__(self):
         return len(self.values)
 
+    #Check for equality of two sets
     def __eq__(self, other):
         return other.getValues == self.values
 
-    def isEmpty(self):
-        return len(self.values) == 0
-
+    #Check if an element is in the set
     def __contains__(self, item):
         return item in self.values.keys()
 
+    #Check if list is empty
+    def isEmpty(self):
+        return len(self.values) == 0
+
+    #Get all of the items in the set
     def getItems(self):
         return list(self.values.keys())
